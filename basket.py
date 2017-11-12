bfed9bca06115557d0cc24895cd06fe05a5eb3d7
Ryan Heard <brotherakuma@gmail.com>
2017-11-12 09:41:51 -0600

    added logging fixed formatting

--- basket.py
+++ basket.py
@@ -1,19 +1,36 @@
 from math import ceil
 import copy
 import json
+import logging
 
-with open('prices.json') as json_data:
-    prices = json.load(json_data)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+handler = logging.FileHandler('basket.log')
+handler.setLevel(logging.INFO)
+
+
+formatter = logging.Formatter('%(asctime)s-%(name)s-%(levelname)s-%(message)s')
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
+
+try:
+    with open('prices.json') as json_data:
+        prices = json.load(json_data)
+    logger.info("Opened prices.json")
+except Exception as e:
+    logger.error(e)
 
 
 class Basket:
     def __init__(self):
         self.items = []
         self.item_objs = []
-        self.disc_cup = {'name': 'BOGO', 'disc': -11.23}
-        self.disc_apple_bag = {'name': 'APPL', 'disc': -4.50}
-        self.disc_oatmeal = {'name': 'APOM', 'disc': "%50"}
-        self.disc_milk = {'name': 'CHMK', 'disc': -4.75}
+        self.disc_cup = {'name': 'BOGO', 'disc': 11.23, 'visual': "-11.23"}
+        self.disc_apple_bag = {'name': 'APPL', 'disc': 4.50, 'visual': "-4.50"}
+        self.disc_oatmeal = {'name': 'APOM', 'disc': .5, 'visual': "-50%"}
+        self.disc_milk = {'name': 'CHMK', 'disc': 4.75, 'visual': "-4.75"}
 
     def print_cart(self):
         total = self.total()
@@ -29,20 +46,22 @@ class Basket:
             # If price is different than item
             if item['Price'] != prices[item['Code']]['Price']:
                 tmp_disc = self.get_discount_code(item)
-                data += "{: >0} {: >10} {: >12.2f}\n".format(" ",
+                data += "{: >0} {: >10} {: >12}\n".format(" ",
                                                              tmp_disc['name'],
-                                                             tmp_disc['disc'])
+                                                             tmp_disc['visual'])
 
         data += "-"*26
         data += "\n"
-        data += "{: >0} {: >10} {: >10.2f}\n".format("...", " ",  total)
+        data += "{: >4} {: >10} {: >8}\n".format("...", " ",  total)
         data += "\n\n"
 
+        logger.info("printed cart")
         print(data)
 
     def add_items(self, items):
         items = [i.strip() for i in items.split(',')]
         self.items.extend(items)
+        logger.info("Added {} to cart".format(items))
 
     def total(self):
         total = 0
@@ -86,13 +105,16 @@ class Basket:
         for item in self.item_objs:
             total += item['Price']
 
+        logger.info("Got total:{}".format(total))
         return total
 
     def check_out(self):
         self.items = []
         self.item_objs = []
+        logger.info("Emptied Cart")
 
     def get_discount_code(self, item):
+        logger.info("Discount on {} ".format(item['Code']))
         if item['Code'] == 'AP1':
             if item['Price'] == self.disc_apple_bag['disc']:
                 return self.disc_apple_bag
